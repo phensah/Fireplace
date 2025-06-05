0aaa566db015a29191c3e3f6e24d3fe2bcb0049e
phensah <phensahs@gmail.com>
2025-06-05 11:43:43 +0530

    minor changes

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const helmet = require('helmet');
 const usersRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
-// process.env.DB_URL
+const dbUrl = process.env.DB_URL;
+// 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl);
 
